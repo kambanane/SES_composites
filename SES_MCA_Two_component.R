ac6a8122f6582e9791221946f3097ac7901bbba4
kambanane <40464694+kambanane@users.noreply.github.com>
2022-01-16 18:01:13 -0500

    Updates

--- SES_MCA_Two_component.R
+++ SES_MCA_Two_component.R
@@ -30,18 +30,21 @@ ses_data_only_asset_columns <- ses_data %>%
 
 #### Check for missingness ####
 
-aggr(ses_data_only_asset_columns, numbers = TRUE, prop = c(TRUE, FALSE))
+aggr(ses_data_only_asset_columns,
+     numbers = TRUE,
+     prop = c(TRUE, FALSE))
 
 ## No missing data, proceed
 # apply mca
 mca = mca(ses_data_only_asset_columns, nf = 5)
 
-### Plot the results of the MCA to see what it all looks like 
+### Plot the results of the MCA to see what it all looks like
 
 # data frame for ggplot
 
 ## Set number of categories for each variable, though in this case there are only two.
-cats = apply(ses_data_only_asset_columns , 2, function(x) nlevels(as.factor(x)))
+cats = apply(ses_data_only_asset_columns , 2, function(x)
+  nlevels(as.factor(x)))
 
 ### Put this all into a data frame
 mca2_vars_df = data.frame(mca$cs, Variable = rep(names(cats), cats))
@@ -51,27 +54,49 @@ mca1_obs_df = data.frame(mca$ind$coord)
 
 
 # plot to see if what we are doing matches our intuition. Does it?
-ggplot(data = mca2_vars_df, 
-       aes(x = X1, y = X2, label = rownames(mca2_vars_df))) +
+ggplot(data = mca2_vars_df,
+       aes(
+         x = X1,
+         y = X2,
+         label = rownames(mca2_vars_df)
+       )) +
   geom_hline(yintercept = 0, colour = "gray70") +
   geom_vline(xintercept = 0, colour = "gray70") +
   geom_text(aes(colour = Variable)) +
-  ggtitle("Loadings for the first two MCA components of Ecodess SES by variable")
+  ggtitle("Loadings for the first two MCA components of MCA based SES by variable")
 
 
 
 #### Add first component back to original data file to get our asset index
-ses_data$ses_mca_1 <- mca$rs[,1]
+ses_data$ses_mca_1 <- mca$rs[, 1]
 
-#### Add second component 
-ses_data$ses_mca_2 <- mca$rs[,2]
+#### Add second component
+ses_data$ses_mca_2 <- mca$rs[, 2]
 
 #### Sum both of these ####
 ses_data$ses_mca <- ses_data$ses_mca_1 + ses_data$ses_mca_2
 
 #### Cut into categories and rename ####
 ses_data$ses_categories <- quantcut(ses_data$ses_mca, 5)
-levels(ses_data$ses_categories) <- c("5 (Wealthiest)", "4","3","2", "1 (Poorest)")
+levels(ses_data$ses_categories) <-
+  c("5 (Wealthiest)", "4", "3", "2", "1 (Poorest)")
 
 ## Summarize
 summary(ses_data)
+
+### Plot summed components with component 1 or 2
+ggplot(data = ses_data,
+       aes(x = ses_mca, y = ses_mca_1)) +
+  geom_point() +
+  ggtitle("MCA two component score with component 1") +
+  xlab("Two component SES composite") +
+  ylab("MCA component 1")
+
+
+### Plot summed components with component 1 or 2
+ggplot(data = ses_data,
+       aes(x = ses_mca, y = ses_mca_2)) +
+  geom_point() +
+  ggtitle("MCA two component score with component 2") +
+  xlab("Two component SES composite") +
+  ylab("MCA component s")
